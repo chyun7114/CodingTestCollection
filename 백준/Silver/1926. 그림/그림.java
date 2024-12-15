8f5a54f967f8cd51e712fe59e4c50425edcc3a70
Luminous <77336660+chyun7114@users.noreply.github.com>
2024-12-15 20:25:02 +0900

    [Silver I] Title: 그림, Time: 328 ms, Memory: 53516 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 지우지 말것 -> 자주 쓰임
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+
+    // 알고리즘 풀이용 static 변수
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] countArr;
+    static int pictureArea;
+    static int maxPictureArea = 0;
+    static int pictureCount = 0;
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        // 그래프
+        map = new int[n][m];
+        visited = new boolean[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 풀이 시작
+
+        // 모든 정점에 대해서 방문할 예정
+        // 만약 방문하는 정점이 1인 경우에 dfs or bfs 탐색 시작
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                // 먼저 그림의 개수를 구할 수 있는지 확인
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    // dfs 시작전
+                    pictureArea = 0;
+
+                    // dfs
+                    dfs(i, j);
+
+                    // 탐색 종료 후 그림 개수 1증가
+                    pictureCount++;
+
+                    // 가장 넓이가 큰 영역 반환
+                    if(maxPictureArea < pictureArea){
+                        maxPictureArea = pictureArea;
+                    }
+                }
+            }
+        }
+
+        System.out.println(pictureCount);
+        System.out.println(maxPictureArea);
+    }
+
+    private static void dfs(int x, int y){
+        // 방문 여부 체크
+        visited[x][y] = true;
+
+        // 색칠 될 때마다 넓이를 1씩 올린다.
+        pictureArea++;
+
+        for(int i = 0; i < dx.length; i++){
+            // 상하좌우 체크
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            // 만약 값이 입력 영역을 넘어간 경우
+            if(nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length)
+                continue;
+
+            // 색이 칠해져 있고 아직 방문하지 않은 경우
+            if(map[nx][ny] == 1 && !visited[nx][ny]){
+                dfs(nx, ny);
+            }
+        }
+    }
+}
