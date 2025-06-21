bc8bfd27adc4564abb28915ba34e6fce45bb97de
Luminous <77336660+chyun7114@users.noreply.github.com>
2025-06-21 21:39:11 +0900

    K번째수.java using java stream

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,21 +4,10 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int a = 0; a < commands.length; a++){
-            int i = commands[a][0];
-            int j = commands[a][1];
-            int k = commands[a][2];
-            
-            int[] temp = new int[j - i + 1];
-            int idx = 0;
-            
-            for(int l = i - 1; l < j; l++){
-                temp[idx] = array[l];
-                idx++;
-            }
-            
+        for(int i = 0; i < commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(temp);
-            answer[a] = temp[k - 1];
+            answer[i] = temp[commands[i][2] - 1];
         }
         
         return answer;
