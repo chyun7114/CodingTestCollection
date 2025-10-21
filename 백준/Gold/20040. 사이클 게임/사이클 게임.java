8e7d94cc34c8efd1253af378b4bd1c9589e1360e
Luminous <77336660+chyun7114@users.noreply.github.com>
2025-10-21 20:27:54 +0900

    [Gold IV] Title: 사이클 게임, Time: 592 ms, Memory: 124924 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 고정 변수들
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    // 변수들 지워도 됨
+    static int n, m;
+    static int[] parent;
+
+
+    static void solveProblem() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (find(x) == find(y)) {
+                System.out.println(i + 1);
+                return;
+            }else {
+                union(x, y);
+            }
+        }
+
+        System.out.println(0);
+    }
+
+    static int find(int x) {
+        if(x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x < y) {
+            parent[y] = x;
+        } else {
+            parent[x] = y;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        solveProblem();
+    }
+}
