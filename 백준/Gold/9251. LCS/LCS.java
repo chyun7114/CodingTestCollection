effea007b69d5ad06f03ee731901a8c20ee01494
Luminous <77336660+chyun7114@users.noreply.github.com>
2025-11-11 17:55:55 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 18284 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 고정 변수들
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    // 변수들 지워도 됨
+    static void solveProblem() throws IOException {
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[s1.length + 1][s2.length + 1];
+
+        for (int i = 1; i <= s1.length; i++) {
+            for (int j = 1; j <= s2.length; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[s1.length][s2.length]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solveProblem();
+    }
+}
